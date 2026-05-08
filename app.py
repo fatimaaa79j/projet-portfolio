e511ef1fb4bb10c3afc68892381d2c8ddb70203e
fatimaaa79j <fatiman3794820@gmail.com>
2026-05-08 12:05:00 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,124 @@
+import streamlit as st 
+st.set_page_config(page_title = "FATOUMATA NDIAYE") 
+
+
+
+st.markdown("<h1 style='rye: center; color: #4A90E2; font-size: 50px, font-weight:bold;'>FATOUMATA NDIAYE</h1>", unsafe_allow_html=True)
+
+st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+
+st.image("lajolie.jpeg",width = 600)
+
+           
+st.subheader("Technicienne supérieure en sciences agricoles et alimentaires")
+st.markdown(" ")
+st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+
+
+
+with st.sidebar:
+    st.title("L'AGRICULTURE, MA PASSION")
+    st.markdown("""<style>[data-testid="stSidebar"]{width: 700px; min-width: 700px; max-width: 700px;}</style>""",unsafe_allow_html=True)
+    
+    st.markdown("""<style>[data-testid="stSidebar"]{background-color: terracotta;}[data-testid="stSidebar"]*{color: white;} </style>""",unsafe_allow_html=True)
+    st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+    st.sidebar.markdown("<h1 style='color: skyblue; font-size: 40px; font-weight:bold;'> Acceuil </h1>", unsafe_allow_html=True)
+    st.title("Bienvenue sur ma page d'acceuil")
+    st.write("Passionnée par la production végétale durable,la production animale et l’innovation agricole. Intéressée par l’amélioration des systèmes agricoles, la sécurité alimentaire et les techniques modernes comme l'hydroponie . Curieuse , Sérieuse, organisée et motivée, je suis une personne travailleuse avec un bon sens de l’observation. Je fais preuve de rigueur dans mes activités et je m’adapte facilement au travail en équipe comme en autonomie.")
+   
+    
+    
+    st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+    st.sidebar.markdown("<h1 style='color: skyblue; font-size: 40px; font-weight:bold;'> Education </h1>", unsafe_allow_html=True)
+    st.title("Cursus scolaire ")
+                                                                      
+    st.markdown("<h1 style='rye: center; color: skyblue; font-size: 22px, font-weight:bold;'> 🎓Licence en Agronomie au DSTAAN</h1>", unsafe_allow_html=True)
+    st.write("Universite Ahmadou Mahtar Mbow , Dakar , 2025-2026")
+    st.markdown("                                                                                                                                                                                                              ")
+    st.markdown("<h1 style='rye: center; color: skyblue; font-size: 22px, font-weight:bold;'> 🎓Diplome du Baccalauréat</h1>", unsafe_allow_html=True)
+    st.write("Groupe Scolaire Emergence Lux , Dakar , 2023-2024")
+
+
+
+    st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+    st.sidebar.markdown("<h1 style='color: skyblue; font-size: 40px; font-weight:bold;'>  Expérience</h1>", unsafe_allow_html=True)
+    st.title("Mon expérience professionnel ")
+    st.markdown("<h1 style='rye: center; color: skyblue; font-size: 22px, font-weight:bold;'> 🌿Stage en exploitation agricole dans Les Jardins de Gouloumbou ", unsafe_allow_html=True)
+    st.write("Tambacounda , Août 2025 - Septembre 2025.")
+    st.write("-Préparation des sols semis et repiquage des cultures" )
+    st.write("-Arrosage et gestion de l’irrigation")
+    st.write("-Entretien des plantes (désherbage, taille, tuteurage)")
+    st.write("-Suivi de la croissance des cultures")
+    st.write("-Observation des maladies et ravageurs")
+    st.write("-Application de techniques de lutte biologique ou traitements phytosanitaires supervisés")
+    st.write("-Récolte des produits agricoles")
+
+
+
+
+
+   
+    st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+    st.sidebar.markdown("<h1 style='color: skyblue; font-size: 40px; font-weight:bold;'> Compétences</h1>", unsafe_allow_html=True)
+ 
+
+    st.title("Mes Compétences")
+    st.markdown("<h1 style='rye: center; color: skyblue; font-size: 22px, font-weight:bold;'> J'ai des compétences en :", unsafe_allow_html=True)
+    st.markdown("-Techniques agricoles (semis, entretien, récolte)")
+    st.markdown("-Analyse des sols et suivi des cultures")
+    st.markdown("-Maîtrise des suites bureautiques")
+    st.markdown("-Maîtrise des systèmes d’irrigation")
+    st.markdown("-Travail en équipe et sens de l’organisation")
+
+
+
+    st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+    st.sidebar.markdown("<h1 style='color: skyblue; font-size: 40px; font-weight:bold;'> Centres d’intérêt </h1>", unsafe_allow_html=True)
+
+    st.title("Ce qui m'interesse vraiment")
+    st.write(" -Les plantes , leur évolution et leur utilité ") 
+    st.write(" -Les animaux , leur mode de vie et leurs ressources ")
+    st.write(" -La photographie ")
+    st.write(" -La pisciculture , les cultures hydroponiques ")
+    st.write(" -Les cultures  fourragères et les cultures maraichères   ")
+    st.write(" -Le micro-jardinage  ")
+    st.write(" -La création d'engrais bio et de biopesticides  ")
+    st.write(" -La promotion du consommé  local ")
+
+
+
+    st.markdown('<hr style="border: 4px solid #1E90FF;">',unsafe_allow_html=True)
+    st.sidebar.markdown("<h1 style='color: skyblue; font-size: 40px; font-weight:bold;'> Contact </h1>", unsafe_allow_html=True)
+    
+    
+        
+       
+        
+    st.markdown("<h1 style='rye: center; color: skyblue; font-size: 22px, font-weight:bold;'> ADRESSE", unsafe_allow_html=True)
+    st.button("Keur Massar",icon=":material/home:")
+    st.markdown("<h1 style='rye: center; color: skyblue; font-size: 22px, font-weight:bold;'> COMPTE MAIL  ", unsafe_allow_html=True) 
+    st.button("ndiaye.fatoumata4@uam.edu.sn",icon=":material/mail:")
+
+        
+    st.markdown(" ")
+
+    st.markdown(" ")
+    st.write(":material/info: Ce portfolio est évolutif donc n’hésitez pas à venir voir les prochaines modifications   , merci 🙂.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
